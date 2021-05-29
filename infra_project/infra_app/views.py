e705c43e2b882020e0c3b4c6bb4593d0ed520ec1
Kreyvi <tankoed@yandex.ru>
2021-05-29 21:46:25 +0300

    deploy1

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse(content='У меня получилось!!! Online)')
+    return HttpResponse(content='У меня получилось!!! Online))')
 
 
 def second_page(request):
